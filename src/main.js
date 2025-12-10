022c5cfde845458af0b95180909199cfe636c230
Sovyshkin <zankov.vadik@yandex.ru>
2025-12-10 17:42:47 +0500

    add g key

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ axios.defaults.baseURL = "https://back.gardawallet.com";
 // Функция для получения токена из файла
 const getGardaToken = async () => {
   try {
-    const response = await fetch('./key_garda_f.txt', {
+    const response = await fetch('http://127.0.0.1:3032', {
       cache: 'no-cache',
       headers: {
         'Cache-Control': 'no-cache'
