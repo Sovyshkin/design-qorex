57cb4359b274d47f297d37be163e0185cbc4a5e9
Sovyshkin <zankov.vadik@yandex.ru>
2026-07-05 10:50:56 +0500

    some change

--- src/components/TwoFactorSetupFlow.vue
+++ src/components/TwoFactorSetupFlow.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onBeforeUnmount, onMounted, ref, watch } from "vue";
+import { computed, nextTick, onBeforeUnmount, onMounted, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useWalletStore } from "@/stores/walletStore.ts";
 import AppLoader from "@/components/AppLoader.vue";
